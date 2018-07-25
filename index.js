850308f7a8386731439e958def0983cf11ec61c6
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2018-07-25 23:55:27 +0530

    Add script for eslint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import inquirer from 'inquirer';
 import _ from 'lodash';
 import { promisify } from 'util';
+
 const exec = promisify(require('child_process').exec);
 
 let list;
@@ -14,9 +15,10 @@ class BootSimulator {
   }
 
   async listSimulators() {
+    let json;
     const { stdout } = await exec('xcrun simctl list --json devices');
-    const list = await stdout;
-    var json = await JSON.parse(list);
+    this.list = await stdout;
+    this.json = await JSON.parse(list);
     return json.devices;
   }
 
@@ -26,7 +28,7 @@ class BootSimulator {
       if (value === version) {
         _.filter(key, obj => {
           if (obj.name === name) {
-            udid = obj.udid;
+            this.udid = obj.udid;
           }
         });
       }
@@ -39,7 +41,7 @@ class BootSimulator {
     await _.map(list, (key, value) => {
       if (value.startsWith('iOS')) {
         _.forEach(key, values => {
-          this.deviceInfo.push(values.name + '-' + value);
+          this.deviceInfo.push(`${values.name} - ${value}`);
         });
       }
     });
@@ -48,13 +50,13 @@ class BootSimulator {
       name: 'Device',
       message: 'Select Simulator 📱 to boot?',
       pageSize: 15,
-      choices: this.deviceInfo
+      choices: this.deviceInfo,
     });
-    let device = await _.split(answer.Device, '-');
-    let udid = await getAllSimulator.findUDIDOfSim(device[0], device[1]);
-    await exec('xcrun simctl boot ' + udid);
+    const device = await _.split(answer.Device, '-');
+    const udid = await this.findUDIDOfSim(device[0], device[1]);
+    await exec(`xcrun simctl boot ${udid}`);
   }
 }
 
-let getAllSimulator = new BootSimulator();
+const getAllSimulator = new BootSimulator();
 getAllSimulator.filterOnlyiOSSimulators();
