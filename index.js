b47305ab92632a1884a5088a821f227132c50a3d
saikrishna321 <saikrishna321@yahoo.com>
2018-11-18 23:05:26 +0000

    format code

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 
-import inquirer from 'inquirer';
-import _ from 'lodash';
-import { promisify } from 'util';
+import inquirer from "inquirer";
+import _ from "lodash";
+import { promisify } from "util";
 
-const exec = promisify(require('child_process').exec);
+const exec = promisify(require("child_process").exec);
 
 let list;
-let deviceparam = '';
-const simbootParams = require('optimist').argv;
+let deviceparam = "";
+const simbootParams = require("optimist").argv;
 
 class BootSimulator {
   constructor() {
     this.deviceInfo = [];
     this.iOSDevices = [];
-    if(simbootParams.d){
+    if (simbootParams.d) {
       deviceparam = simbootParams.d;
     }
   }
 
   async listSimulators() {
-    const { stdout } = await exec('xcrun simctl list --json devices');
+    const { stdout } = await exec("xcrun simctl list --json devices");
     this.list = await stdout;
     this.json = await JSON.parse(this.list);
     return this.json.devices;
@@ -43,33 +43,36 @@ class BootSimulator {
     let device;
     list = await this.listSimulators();
     await _.map(list, (key, value) => {
-      if (value.startsWith('iOS')) {
+      if (value.startsWith("iOS")) {
         _.forEach(key, values => {
-          if(values.name.replace(/ /g,'').toUpperCase().includes(deviceparam.replace(/ /g,'').toUpperCase()))
+          if (
+            values.name
+              .replace(/ /g, "")
+              .toUpperCase()
+              .includes(deviceparam.replace(/ /g, "").toUpperCase())
+          )
             this.deviceInfo.push(`${values.name}-${value}`);
         });
       }
     });
-    if(this.deviceInfo.length<1){
+    if (this.deviceInfo.length < 1) {
       console.log(`No device matches with provided device name`);
-      device = '';
-    }
-    else if(this.deviceInfo.length > 1){
+      device = "";
+    } else if (this.deviceInfo.length > 1) {
       const answer = await inquirer.prompt({
-        type: 'list',
-        name: 'Device',
-        message: 'Select Simulator 📱 to boot?',
+        type: "list",
+        name: "Device",
+        message: "Select Simulator 📱 to boot?",
         pageSize: 15,
-        choices: this.deviceInfo,
-     });
-      device = await _.split(answer.Device, '-');
-    }
-    else{
-      device = await _.split(this.deviceInfo[0], '-');
+        choices: this.deviceInfo
+      });
+      device = await _.split(answer.Device, "-");
+    } else {
+      device = await _.split(this.deviceInfo[0], "-");
     }
 
     const udid = await getAllSimulator.findUDIDOfSim(device[0], device[1]);
-    if(udid){
+    if (udid) {
       console.log(`Booting ${device[0]} with ${device[1]}`);
       await exec(`xcrun simctl boot  ${udid}`);
       await exec(`open -a Simulator --args -CurrentDeviceUDID ${udid}`);
